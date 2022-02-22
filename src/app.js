f51da8f68dc46092cea5306c0694f62c3079f60a
Jorge22fv <jorge.22fv@gmail.com>
2022-02-22 01:40:37 +0000

    random card 98%

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♦", "♥", "♠", "♣"];
+  const mix = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let suitNumber = Math.floor(Math.random() * suits.length);
+  let mixNumber = Math.floor(Math.random() * mix.length);
+
+  document.querySelector(".mix").innerHTML = mix[mixNumber];
+  document.querySelector(".suit1").innerHTML = suits[suitNumber];
+  document.querySelector(".suit2").innerHTML = suits[suitNumber];
+
+  if (suits[suitNumber] == "♦" || suits[suitNumber] == "♥") {
+    document.querySelector(".suit1").style.color = "red";
+    document.querySelector(".suit2").style.color = "red";
+    document.querySelector(".mix").style.color = "red";
+  }
 };
